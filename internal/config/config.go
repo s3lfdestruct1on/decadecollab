ac22f67670c0fa6a3723a185fa1d4bc88c094388
Sergey Palshin <filroy.gamer@gmail.com>
2025-04-08 23:55:33 +0700

    add PORT to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,7 @@ type Config struct{
 }
 
 type PSQLConfig struct{
+	Port string `env:"PORT,required"`
 	Username string `env:"USER,required"`
 	Password string	`env:"PASS,required"`
 	Database string `env:"DB,required"`
