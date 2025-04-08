91b2517779075dfa27a98b1b3362822b5ef9b4f1
Sergey Palshin <filroy.gamer@gmail.com>
2025-04-08 23:27:26 +0700

    make config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,11 @@
 package config
 
 type Config struct{
-	
+	AppPort  int    `env:"PORT,required,omitempty"`
+}
+
+type PSQLConfig struct{
+	Username string `env:"USER,required,omitempty"`
+	Password string	`env:"PASS,required,omitempty"`
+	Database string `env:"DB,required,omitempty"`
 }
