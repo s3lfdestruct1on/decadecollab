ea5117a1350779bcd13b32fc11bedeafeabae318
anton <smoke2.nvkz@gmail.com>
2025-04-10 00:16:36 +0700

    fixed conn

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,7 +6,7 @@ type Config struct{
 
 type PSQLConfig struct{
 	Port string `env:"PORT,required"`
-	Username string `env:"USER,required"`
+	Username string `env:"USERDB,required"`
 	Password string	`env:"PASS,required"`
 	Database string `env:"DB,required"`
 }
--- internal/storage/sql/postgre.go
+++ internal/storage/sql/postgre.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/jackc/pgx/v5"
 )
-
 //sdelai logger :D
 func CreateUsersTable(conn *pgx.Conn) error {
     query := `
