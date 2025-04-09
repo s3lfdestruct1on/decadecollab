939af5bc48899672f5465fa77d736720072d1bcb
Sergey Palshin <filroy.gamer@gmail.com>
2025-04-09 23:40:33 +0700

    ErrOpts add

--- internal/lib/logger/sl/sl.go
+++ internal/lib/logger/sl/sl.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	//"errors"
 )
+//функции *Opts принимают не только строку/ошибку, но и атрибуты
 
 func Debug(msg string) {
 	logger := slog.New(prettylog.NewHandler(nil))
@@ -12,7 +13,6 @@ func Debug(msg string) {
 	logger.Debug(msg)
 }
 
-//функции *Opts принимают не только строку/ошибку, но и атрибуты
 
 func DebugOpts(msg string, opts *slog.HandlerOptions) {
 	logger := slog.New(prettylog.NewHandler(opts))
@@ -39,11 +39,15 @@ func Err(err error){
 	logger.Error("We have some problems", "error", err)
 }
 
+func ErrOpts(err error, opts *slog.HandlerOptions){
+	logger := slog.New(prettylog.NewHandler(opts))
 
+	logger.Error("We have some problems", "error", err)
+}
 //эта функция тест обработки ошибки с атрибутами
 //на ReplaceAttr происходит перезапись стандартных аттрибутов нашими
 //если a.Key пустой, то не делаем ничего 
-func TestErrOpts(err error, ){     
+func TestErrOpts(err error){     
 	opts := &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 		AddSource: true,
