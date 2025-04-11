2dc7ba08b127f5440ac5c5ce41e8bee928b1ac81
Sergey Palshin <filroy.gamer@gmail.com>
2025-04-11 23:30:27 +0700

    do some test things

--- internal/server/handlers/test.go
+++ internal/server/handlers/test.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"decadecollab/internal/lib/logger/prettylog"
+	//"decadecollab/internal/lib/logger/sl"
+	"log/slog"
 	"net/http"
 	"time"
 
@@ -8,5 +11,14 @@ import (
 )
 
 func CurrentTime(c echo.Context) error {
+	prikol := slog.HandlerOptions{
+
+		AddSource: true,
+	}
+
+	logger := slog.New(prettylog.NewHandler(&prikol))
+	
+	logger.Info("tam po CurrentTime postuchali")
   	return c.JSON(http.StatusOK, map[string]any{"current_time":time.Now().Format("2006-01-02 15:04:05")})
+	
 }
