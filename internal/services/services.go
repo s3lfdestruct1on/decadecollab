dc9132cd3c41a6caf668ce54445e455aa6a8ce72
anton <smoke2.nvkz@gmail.com>
2025-04-12 01:32:56 +0700

    hotfix

--- internal/services/services.go
+++ internal/services/services.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 )
 
-
+// возвращает одного юзера по айди
 func GetUser(id int)*models.User{
 	if id <= 0{
 		fmt.Errorf("invalid id")
@@ -21,39 +21,39 @@ func GetUser(id int)*models.User{
 func InitDB(){
 	postgre.InitDB()
 }
-
-func NewUser(username string,password string,email string)int{
-	id,_ := postgre.CreateUser(username,password,email)
+//создает пользователя с валидацей, смотреть условия
+func NewUser(u *models.User)int{
+	id,_ := postgre.CreateUser(u)
 	return id
 }
 
-
+//удаляет юзера по айди id = user_id
 func DeleteUSer(id int){
 	postgre.DeleteUser(id)
 }
-
+// возвращает всех пользователй в диапазоне from - to, e.g. from 100 to 150 вернет 50 пользователей начиная с 100
 func GetUsersFromTo(from int,to int)[]models.User{
 	res,_ := postgre.GetUsersFromTo(from,to)
 	return res
 }
-
+// возвращает страницу(10) пользователей
 func GetUsersPaging(page int)[]models.User{
 	res,_ := postgre.GetUsersPaging(page)
 	return res
 }
-
-func UpdateUser(id int,username string,password string,email string){
-	postgre.UpdateUser(id,username,password,email)
+// апдейт с валидацией полностью пользователя,смотреть условия
+func UpdateUser(id int,u *models.User){
+	postgre.UpdateUser(id,u)
 }
-
+// смена только юзернейма с валидацией id = user_id, смотреть условия
 func UpdateUsername(id int,username string){
 	postgre.UpdateUsername(id,username)
 }
-
+// смена пароля с валидацией id = user_id
 func UpdatePassword(id int,password string){
 	postgre.UpdateUsername(id,password)
 }
-
+// смена только имейла с валидацией, id = user_id
 func UpdateEmail(id int,email string){
 	postgre.UpdateUsername(id,email)
 }
