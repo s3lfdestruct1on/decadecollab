5e628e58f0bd1acd0b2efdc7d044c6cfb00b1860
Sergey <53634824+Katayo0@users.noreply.github.com>
2025-04-12 00:15:22 +0700

    Update test.go

--- internal/server/handlers/test.go
+++ internal/server/handlers/test.go
@@ -1,1 +1,21 @@
+import (
+	"decadecollab/internal/lib/logger/prettylog"
+	"log/slog"
+	"net/http"
+	"time"
 
+	"github.com/labstack/echo/v4"
+)
+
+func CurrentTime(c echo.Context) error {
+	prikol := slog.HandlerOptions{
+
+		AddSource: true,
+	}
+
+	logger := slog.New(prettylog.NewHandler(&prikol))
+	
+	logger.Info("tam po CurrentTime postuchali")
+  	return c.JSON(http.StatusOK, map[string]any{"current_time":time.Now().Format("2006-01-02 15:04:05")})
+	
+}
