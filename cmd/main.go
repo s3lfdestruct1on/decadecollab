0f93b3bb38f109cc3fb811419f8f8c6480401c8f
Sergey Palshin <filroy.gamer@gmail.com>
2025-04-16 01:54:15 +0700

    update initdb function

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"decadecollab/internal/config"
 	"decadecollab/internal/lib/logger/sl"
 	"decadecollab/internal/models"
+	postgre "decadecollab/internal/storage/sql"
+
+	//"decadecollab/internal/models"
 	"os"
 
 	//postgre "decadecollab/internal/storage/sql"
@@ -19,16 +22,23 @@ var (
   cfg = config.Config{
 	AppHostPort: os.Getenv("HOSTPORT"),
   }
+  testuser = models.User{
+	Username: "2aaaaaaaaaa",
+	Password: "444A",
+	Email: "gg3@gg.gg",
+  }
 )
 func main() {
 
 	
 	
 	server := echo.New()
-
-	apiv1 := server.Group("/api/v1")	
+	postgre.InitDB()
+	apiv1 := server.Group("/api/v1")
+	postgre.CreateUser(&testuser)	
 	apiv1.GET("/time", handlers.CurrentTime)
 	apiv1.GET("/user/:id", handlers.GetUser)
+	
 	if err := server.Start(cfg.AppHostPort); err != nil {
 		sl.PLogger.Error("unable to start server", "error", err.Error())
 	}
--- internal/models/entity.go
+++ internal/models/entity.go
@@ -1,8 +1,13 @@
 package models
 
+import "time"
+
 type User struct{
 	Id int
 	Username string `validate:"required,min=6,max=20,alphanum"`
 	Password string `validate:"required,min=4,max=25,alphanum"`
 	Email string `validate:"required,email"`
+	LastActive time.Time
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
