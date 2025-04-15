2c0e9fbc649fcd0e1dd818140633c2de6a4305c6
anton <smoke2.nvkz@gmail.com>
2025-04-16 04:16:05 +0700

    added models

--- internal/models/entity.go
+++ internal/models/entity.go
@@ -1,13 +1,31 @@
 package models
 
 import "time"
-
+//TODO: review всего этого дерьма сделать
 type User struct{
-	Id int
-	Username string `validate:"required,min=6,max=20,alphanum"`
-	Password string `validate:"required,min=4,max=25,alphanum"`
-	Email string `validate:"required,email"`
-	LastActive time.Time
-	CreatedAt time.Time
-	UpdatedAt time.Time
+  Id int64
+  Username string `validate:"required,min=6,max=20,alphanum"`
+  Password string `validate:"required,min=4,max=25,alphanum"`
+  Email string `validate:"required,email"`
+  LastActive time.Time
+  CreatedAt time.Time
+  UpdatedAt time.Time
+}
+
+type Item struct{
+  Id int64
+  Title string 
+  Stock int64 
+  Price int16
+  SalePercent int16
+  Tags string
+  Description string
+}
+
+type Basket struct{
+  Id int64
+  UserID int64 
+  ItemID int64 
+  TotalPrice int64
+  Quantity int16
 }
